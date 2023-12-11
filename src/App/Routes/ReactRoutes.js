53164e538d6b25396d8c463bf8041072fb233bea
Nauman iftikhar <130370233+MrNauman9@users.noreply.github.com>
2023-12-11 21:50:01 +0100

    Navigates pages and components

--- src/App/Routes/ReactRoutes.js
+++ src/App/Routes/ReactRoutes.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from '../Pages/Home/Home';
-import Layout from './Layout';
-import HouseDetail from '../Pages/HouseDetail/HouseDetail';
-import RoomDetail from '../Pages/RoomDetail/RoomDetail';
+import Home from "../Pages/Home/Home";
+import Layout from "./Layout";
+import HouseDetail from "../Pages/HouseDetail/HouseDetail";
+import RoomDetail from "../Pages/RoomDetail/RoomDetail";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Signup/Signup";
 function ReactRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='houseDetail' element={<HouseDetail />} />
-          <Route path='roomDetail' element={<RoomDetail />} />
+          {localStorage.getItem("user") && (
+            <>
+              <Route index element={<Home />} />
+              <Route path="houseDetail" element={<HouseDetail />} />
+              <Route path="roomDetail" element={<RoomDetail />} />
+            </>
+          )}
+          {!localStorage.getItem("user") && (
+            <>
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+            </>
+          )}
+          <Route path="*" element={localStorage.getItem("user") ? <Home /> : <Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default ReactRoutes
+export default ReactRoutes;
