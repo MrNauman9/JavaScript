10842c185bed6cdf6620e8675399a930b4d52bb0
M-Saqlain-Salat <saqlain_1983@yahoo.com>
2024-08-08 18:36:46 +0200

    Update state mngmnt and event handling

--- src/App/Component/HomePopup/HomePopup.js
+++ src/App/Component/HomePopup/HomePopup.js
@@ -1,92 +1,76 @@
-import React, { useState } from "react";
-import {
-  useAddHomeMutation,
-  useUpdateHomeMutation,
-} from "../../redux/services/DZapi";
-function HomePopup({ popup, setPopup, homeId, setHomeId, userId }) {
-  const [addHome] = useAddHomeMutation();
-  const [updateHome] = useUpdateHomeMutation();
+import React, { useState, useEffect } from "react";
+
+function HomePopup({ selectedHome, onAdd, onUpdate, closePopup }) {
   const [data, setData] = useState({
     address: "",
     deviceEui: "",
+    roomProfiles: [],
   });
+  
+  useEffect(() => {
+    if (selectedHome) {
+      setData({
+        address: selectedHome.address || "",
+        deviceEui: selectedHome.deviceEui || "",
+        roomProfiles: selectedHome.roomProfiles || [],
+      });
+    }
+  }, [selectedHome]);
+
   const handleChange = (e) => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
     });
   };
-  const closePopup = () => {
-    setPopup(false);
-    setHomeId(null);
-  };
-  console.log(userId);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const updateData = {
-      data: {
-        homeId: homeId,
-        address: data.address,
-        deviceEui: data.deviceEui,
-      },
-      userId: userId,
-    };
-    const addData = {
-      data: {
-        address: data.address,
-        deviceEui: data.deviceEui,
-      },
-      userId: userId,
-    };
-    if (homeId) {
-      updateHome(updateData);
-      closePopup();
-      setHomeId(null);
+    if (selectedHome) {
+      onUpdate(selectedHome.id, data);
     } else {
-      addHome(addData);
-      closePopup();
-      setHomeId(null);
+      onAdd(data);
     }
+    closePopup();
   };
+
   return (
-    <>
-      {popup && (
-        <form className="edit__popup">
-          <h1 className="small__title full__area">
-            {homeId ? "Edit" : "Add"} <span>House</span>
-          </h1>
+    <form className="edit__popup" onSubmit={handleSubmit}>
+      <h1 className="small__title full__area">
+        {selectedHome ? "Edit" : "Add"} <span>House</span>
+      </h1>
 
-          <div className="input__field full__area">
-            <span className="input__text">Address</span>
-            <input
-              type="text"
-              name="address"
-              onChange={handleChange}
-              placeholder="Write the Address of the house"
-            />
-          </div>
+      <div className="input__field full__area">
+        <span className="input__text">Address</span>
+        <input
+          type="text"
+          name="address"
+          value={data.address}
+          onChange={handleChange}
+          placeholder="Write the Address of the house"
+        />
+      </div>
 
-          <div className="input__field full__area">
-            <span className="input__text">Device EUI</span>
-            <input
-              type="text"
-              name="deviceEui"
-              onChange={handleChange}
-              placeholder="Write the Device EUI"
-            />
-          </div>
+      <div className="input__field full__area">
+        <span className="input__text">Device EUI</span>
+        <input
+          type="text"
+          name="deviceEui"
+          value={data.deviceEui}
+          onChange={handleChange}
+          placeholder="Write the Device EUI"
+        />
+      </div>
 
-          <div className="seperator">
-            <div className="btn" onClick={handleSubmit}>
-              Submit
-            </div>
-            <div className="btn" onClick={() => closePopup()}>
-              Cancel
-            </div>
-          </div>
-        </form>
-      )}
-    </>
+      <div className="seperator">
+        <button type="submit" className="btn">
+          Submit
+        </button>
+        <div className="btn" onClick={closePopup}>
+          Cancel
+        </div>
+      </div>
+    </form>
   );
 }
 
