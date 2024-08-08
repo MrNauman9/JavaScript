087380c37380859f60e00ecf9da19916b308b473
M-Saqlain-Salat <saqlain_1983@yahoo.com>
2024-08-08 18:43:27 +0200

    Refactor House detail Card Component

--- src/App/Component/HouseDetailCard/HouseDetailCard.js
+++ src/App/Component/HouseDetailCard/HouseDetailCard.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { useDeleteHomeMutation } from "../../redux/services/DZapi";
-function HouseDetailCard({ data,  }) {
-  const [deleteHome] = useDeleteHomeMutation();
+import { Link } from "react-router-dom";
+
+function HouseDetailCard({ data, onDelete, allRooms }) {
   const houseDelete = () => {
-    deleteHome(data?.id);
+    onDelete(data.id);
   };
+
   return (
     <div className="house__detail">
       <div className="header__container">
@@ -12,31 +13,26 @@ function HouseDetailCard({ data,  }) {
         <div className="detail__container">
           <span className="detail__list">
             id
-            <span>01</span>
+            <span>{data?.id}</span>
           </span>
           <span className="detail__list">
-            Device Name
-            <span>{data?.deviceName || "Xyz"}</span>
-          </span>
-          <span className="detail__list">
-            House Address
-            <span>{data?.houseName || "Street 983"}</span>
-          </span>
-          <span className="detail__list">
-            Number of Rooms
-            <span>{data?.numberOfRooms || "10"}</span>
+            Device EUI
+            <span>{data?.deviceEui}</span>
           </span>
           <span className="detail__list">
             Address
-            <span>{data?.address || "24 Street"}</span>
+            <span>{data?.address}</span>
+          </span>
+          <span className="detail__list">
+            Number of Rooms
+            <span>{allRooms?.length || 0}</span>
           </span>
         </div>
       </div>
-      <div className="btns"  >
-        <div className="btn" style={{display: "flex", gap: "10px"}} onClick={houseDelete}>
-          Delete
-        <i className="ri-delete-bin-7-fill"></i>
-        </div>
+      <div className="btns">
+        <Link to="/" className="btn">
+        Go Back to Home
+        </Link>
       </div>
     </div>
   );
