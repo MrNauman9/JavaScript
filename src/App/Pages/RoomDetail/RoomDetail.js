85a361bdd244909817380b2bced7bd0aafb56870
M-Saqlain-Salat <saqlain_1983@yahoo.com>
2024-08-12 00:22:29 +0200

    Bug fix

--- src/App/Pages/RoomDetail/RoomDetail.js
+++ src/App/Pages/RoomDetail/RoomDetail.js
@@ -5,48 +5,39 @@ import { Link, useLocation } from "react-router-dom";
 
 function RoomDetail() {
   const location = useLocation();
-  const { room, home, allRooms} = location?.state
+  const { room, home, allRooms } = location?.state;
+  console.log(room, "room");
   return (
     <div id="mainRoomDetail">
       <div className="house__detail">
         <div className="header__container">
           <h1 className="small__title">Room Details</h1>
-          <div className="detail__container"> 
+          <div className="detail__container">
             <span className="detail__list">
-              id
-              <span>01</span>
-            </span> 
-            <span className="detail__list">
-              Device Name
-              <span>{home?.deviceEui && "Xyz"}</span>
+              Id:
+              <span>{room?.id}</span>
             </span>
             <span className="detail__list">
-              Number of Rooms
-              <span>{allRooms?.length && "10"}</span>
-            </span>
-            <span className="detail__list">
-              Address
-              <span>{home?.address && "24 Street"}</span>
+              Room:
+              <span>{room?.roomName}</span>
             </span>
           </div>
         </div>
 
         <Link to="/" className="btn">
-           Go Back to Home
+          Go Back to Home
         </Link>
       </div>
       <div className="room__container roomDetail__container">
         <h1 className="small__title">
-          Rooms Temperature 
+          Rooms Temperature
           <span>General</span>
         </h1>
         <div className="tabs__container">
-          <a className="tab active">
-            General
-          </a>
+          <a className="tab active">General</a>
         </div>
         <div className="cards__container">
-            <RoomDetailCard  data={room} />;
+          <RoomDetailCard data={room} limits={room?.limits} />;
         </div>
       </div>
     </div>
