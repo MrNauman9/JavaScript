5a27d69109d623a3d84697dbbfd3e29d1fc56bd6
M-Saqlain-Salat <saqlain_1983@yahoo.com>
2024-08-12 02:50:47 +0200

    Updated Props

--- src/App/Component/RoomDetailCard/RoomDetailCard.js
+++ src/App/Component/RoomDetailCard/RoomDetailCard.js
@@ -1,52 +1,26 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-function RoomCard({
-  data,
-  setPopup,
-  setRoomId,
-  onDelete,
-  home,
-  allRooms,
-  room,
-}) {
-  const navigate = useNavigate();
- 
-  const editRoom = (e) => {
-    e.stopPropagation();
-    setRoomId(data.id);
-    setPopup(true);
-  };
-
-  const deleteRoom = async (e) => {
-    e.stopPropagation();
-    await onDelete(data.id);
-  };
-
-  const navigateRoomDetail = () => {
-    navigate("/roomDetail", {
-      state: { roomId: data.id, home: home, allRooms: allRooms, room: room },
-    });
-  };
-
+function RoomDetailCard({limits, data}) {
   return (
-    <div onClick={navigateRoomDetail} className="room__card">
+    <div className="room__card" >
       <div className="card__img">
-        <img src="images/room.png" alt="Room Image" />
-        <span className="room__text">Room Number</span>
+        <img src="images/temprautre.png" alt="Temperature Image"/>
+        <span className="room__text">Room Temperature</span>
       </div>
       <span className="room__text">
-        id
-        <span>{data.id}</span>
+        Min Temperature
+        <span>{limits?.temperatureMin} Degree</span>
       </span>
       <span className="room__text">
-        Room Name
-        <span>{data.roomName}</span>
+        Max Temperature
+        <span>{limits?.temperatureMax} Degree</span>
       </span>
-      <i className="ri-edit-box-fill edit__icon" onClick={editRoom}></i>
-      <i className="ri-delete-bin-7-fill delete__icon" onClick={deleteRoom}></i>
+      <span className="room__text">
+        Current Temperature
+        <span>{data.idealTemperature} Degree</span>
+      </span>
     </div>
   );
 }
 
-export default RoomCard;
+export default RoomDetailCard;
